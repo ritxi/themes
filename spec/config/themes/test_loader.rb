36080d1b3f8358e1f702582109d24d7d18f15828
Ricard Forniol <ricard.forniol@sopranodesign.com>
2020-01-08 09:44:00 +0100

    Remove theme name assignation

--- spec/config/themes/test_loader.rb
+++ spec/config/themes/test_loader.rb
@@ -2,8 +2,6 @@ class TestLoader < Themes::BaseLoader
   HOSTNAMES = %w[test b c].freeze
 
   def self.configure
-    Rails.logger.info 'configure test theme'
-    Themes.name = 'test'
     Themes.email = 'test@test.cat'
   end
 end
