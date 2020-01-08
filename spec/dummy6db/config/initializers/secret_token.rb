e6ff556294369cedefeb5e48f622e9d5ce28c1cb
Ricard Forniol <ricard.forniol@sopranodesign.com>
2020-01-08 10:49:52 +0100

    Add rails 6 dummy app with db configuration

---
+++ spec/dummy6db/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '21eb9ba70ea7174e26cd1795bf9b89266e855c21e92518f29aeae429cc7f21bc02ab111b7f048ad146eebc8900358d492f8b4c651e9020d723402c5d6fa4226d'
